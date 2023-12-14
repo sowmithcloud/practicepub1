69ced762cbe5ae822a932471f898abe739522043
sowmithcloud <sowmithcloud@gmail.com>
2023-12-14 18:16:32 +0530

    installation

---
+++ functioninstall.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+
+TIMESTAMP=$date
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo "Script started executing at $TIMESTAMP" &>> $LOGFILE
+
+validate(){
+    if [ $1 != 0 ]
+    then
+        echo "Error: $2 failed"
+        exit 1
+    else
+        echo "$2 success"
+}
+
+if [ $ID != 0 ]
+then
+    echo "Error: run the script as root user"
+    exit 1
+else
+    echo "You are a root user"
+fi
+
+yum install mysql -y &>> $LOGFILE
+validate $? "Intalling mysql"
+
+yum install git -y &>> $LOGFILE
+validate $? "Intalling git"
--- scriptinstall.sh
+++ scriptinstall.sh
@@ -5,7 +5,7 @@ ID=$(id -u)
 if [ $ID != 0 ]
 then
     echo "Error: Please run the script with root user"
-    exit 1
+    exit 1 # this is will stop the script from running
 else
     echo "You are a root user"
 
