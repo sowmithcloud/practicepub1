033d7a5ec67cbb4cc52a78dec06585f18bbefb4b
sowmithcloud <sowmithcloud@gmail.com>
2023-12-21 15:58:39 +0530

    diskusage

--- getoptsscript.sh
+++ getoptsscript.sh
@@ -18,7 +18,7 @@ do
     case $options in
         n) NAME="$OPTARG";;
         w) WISHES="$OPTARG";;
-       \?) echo "Invalid option: -$OPTARG" >&2; USAGE; exit 1;;
+       \?) echo "Invalid option: -"$OPTARG"" >&2; USAGE; exit 1;;
         :) USAGE; exit 1;;
         h|*) USAGE;
     esac
