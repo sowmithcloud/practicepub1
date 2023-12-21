cf9f498e3e3760d205ce3c227ab4d0f44b39acc6
sowmithcloud <sowmithcloud@gmail.com>
2023-12-21 16:23:28 +0530

    diskusage

--- getoptsscript.sh
+++ getoptsscript.sh
@@ -3,26 +3,22 @@
 NAME=""
 WISHES="Good Morning"
 
-USAGE() {
-
-    echo "USAGE: $(basename $0) -n <name> -w <wishes>"
+USAGE(){
+    echo "USGAE:: $(basename $0) -n <name> -w <wishes>"
     echo "Options::"
-    echo "-n , specify name for this option"
-    echo "-w , specify wish for this option"
-    echo "-h , for help or suggestion"
+    echo " -n, Specify the name (mandatory)"
+    echo " -w, Specify the wishes. (Optional). Default is Good Morning"
+    echo " -h, Display Help and exit"
 }
 
-
-while getopts "n:w:h" options
-do
-    case $options in
+while getopts ":n:w:h" opt; do
+    case $opt in
         n) NAME="$OPTARG";;
         w) WISHES="$OPTARG";;
-        \?) echo "Invalid option: -"$OPTARG"" >&2; USAGE; exit 1;;
+        \?) echo "invalid options: -"$OPTARG"" >&2; USAGE; exit;;
         :) USAGE; exit;;
         h) USAGE; exit;;
     esac
-
 done
 
 if [ -z "$NAME" ] || [ -z "$WISHES" ]
